4b3a9a48cafc8b9fcb61289aafb7e96bc65ee206
Gabriel <galplis190@gmail.com>
2018-12-20 21:09:23 -0200

    Versão Inicial da IC

--- app/src/main/java/com/ordenaisso/gabriel/ordenaisso/TelaPrincipal.java
+++ app/src/main/java/com/ordenaisso/gabriel/ordenaisso/TelaPrincipal.java
@@ -1,12 +1,21 @@
 package com.ordenaisso.gabriel.ordenaisso;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class TelaPrincipal extends AppCompatActivity {
 
@@ -14,10 +23,15 @@ public class TelaPrincipal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_principal);
-        final EditText edtTxt;
+        EditText edtTxt;
+
 
         edtTxt = findViewById(R.id.idEdtView);
+
+
         final int duracao = Toast.LENGTH_SHORT;
+
+
         Button botao_confirma = findViewById(R.id.idBotaoConfirma);
         botao_confirma.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +42,29 @@ public class TelaPrincipal extends AppCompatActivity {
                     toast.show();
                 }else
                 {
-                    String txt = "Valor digitado: " + edtTxt.getText();
+                    String numerosString = edtTxt.getText().toString();
+                    String txt;
+
+                    String[] numeros = numerosString.split(" ");
+                    boolean teste = testaInteiro(numeros);
+                    if (teste) {
+                        List<Integer> list = new ArrayList<Integer>();
+                        for (String numero : numeros) {
+                            list.add(Integer.parseInt(numero));
+                        }
+
+                        int tamLista = 0;
+                        tamLista = list.size();
+                        txt = "Valores digitados: ";
+                        for (int i = 0; i < tamLista; i++) {
+                            txt += list.get(i).toString() + " ";
+
+                        }
+
+                    } else
+                    {
+                        txt = "Digite apenas números inteiros!";
+                    }
                     Toast toast = Toast.makeText(getApplicationContext(), txt, duracao);
                     toast.show();
                 }
@@ -67,5 +103,23 @@ public class TelaPrincipal extends AppCompatActivity {
     {
         Intent intentAlgoritmos = new Intent (this, AlgoritmosActivity.class);
         startActivity(intentAlgoritmos);
+    }
+
+    public boolean testaInteiro(String[] numeros)
+    {
+        boolean testeResultado = true;
+
+        for (String num: numeros)
+        {
+            try
+            {
+                int valor = Integer.parseInt(num);
+            } catch(NumberFormatException e)
+            {
+                testeResultado = false;
+            }
+        }
+
+        return testeResultado;
     }
 }
